8989fc68e805cb2417adc89a47774efa2ac7dfcc
Robin Afflerbach <robin.afflerbach@invers.com>
2017-04-24 13:20:58 +0200

    add dynamic filter argument for the source collection as JSON object

--- elasticize.py
+++ elasticize.py
@@ -1,11 +1,37 @@
 import requests
 import pymongo
+import sys
+import re
+import isodate
 from pymongo import MongoClient
 import json
 from datetime import datetime
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 config = json.loads(open('config.json', 'r').read())
+
+def is_iso_date_string(date_str):
+	iso_date_pattern = '^(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))$'
+	res = re.search(iso_date_pattern, date_str)
+	return True if res != None else False
+
+def convert_date_strings_to_dates(obj):
+	for key in obj:
+		if isinstance(obj[key], (str, unicode)):
+			if is_iso_date_string(obj[key]):
+				obj[key] = isodate.parse_datetime(obj[key])
+		elif isinstance(obj[key], dict):
+			convert_date_strings_to_dates(obj[key])
+
+collectionfilter = {};
+if len(sys.argv) > 1:
+	try:
+		collectionfilter = json.loads(sys.argv[1])
+	except:
+		print('Could not parse collection filter')
+		pass
+
+convert_date_strings_to_dates(collectionfilter)		
 
 connection = pymongo.MongoClient()
 db = connection[config['MONGO_DATABASE']]
@@ -15,7 +41,7 @@ def outputJSON(obj):
     if isinstance(obj, datetime):
         return obj.strftime('%Y-%m-%dT%H:%M:%S')
 
-collection = db[config['MONGO_COLLECTION']].find({})
+collection = db[config['MONGO_COLLECTION']].find(collectionfilter)
 documents = []
 
 print("Creating documents...")
